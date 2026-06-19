5da11b772570e9b4d21a83c5349ed8acb377d502
Naoman Saeed <naoman.saeed@gmail.com>
2026-06-19 10:32:29 +0500

    javascript type coersion and truthiness.

--- script.js
+++ script.js
@@ -13,7 +13,32 @@ console.log("Outside block: ", a, b, typeof c);
 
 // ----
 const myArr = [3, 5, 7];
-myArr = [2, 4, 6]; // failure expected
+//myArr = [2, 4, 6]; // failure expected
 console.log(myArr);
 myArr[1] = 17;
 console.log(myArr);
+
+// Type coersion
+console.log(typeof null);
+console.log([] == false);
+console.log("10" + 5);
+console.log("10" - 5);
+console.log(0.1 + 0.2 === 0.3);
+
+// function to check truthyness
+function isTruthy(value) {
+  if (value === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isTruthy(0));
+console.log(isTruthy(""));
+console.log(isTruthy(null));
+console.log(isTruthy(undefined));
+console.log(isTruthy(NaN));
+console.log(isTruthy("hello"));
+console.log(isTruthy([]));
+console.log(isTruthy({}));
