cfe7c48f60bc8751de7b6fcfe8af1407335b3fe7
Naoman Saeed <naoman.saeed@gmail.com>
2026-06-24 08:24:06 +0500

    practicing functions basics.

--- functions.js
+++ functions.js
@@ -37,3 +37,45 @@ const object = {
 };
 const detached = object.log;
 detached();
+
+function calculateArea(radius) {
+  if (typeof radius !== "number" || radius <= 0) {
+    console.error(`Error: radius must be a positive number`);
+    return null;
+  }
+  const pi = 3.14159;
+  const area = pi * radius * radius;
+  return area;
+}
+/*
+function recieveInput() {
+
+  do {const r = prompt(`Enter radius: ${r}`); } while (r ===);
+  } */
+
+console.log(calculateArea(7));
+console.log(calculateArea(-3));
+console.log(calculateArea("10"));
+
+function calculateDiscount(originalPrice, discountPercent) {
+  if (typeof originalPrice !== "number" || originalPrice < 0) {
+    console.error(`Error: original price must be a non negative number.`);
+    return null;
+  }
+  if (
+    typeof discountPercent !== "number" ||
+    discountPercent < 0 ||
+    discountPercent > 100
+  ) {
+    console.error(`Error: Discount Percent should be between 0 and 100.`);
+    return null;
+  }
+
+  const finalPrice = originalPrice * discountPercent;
+  return finalPrice;
+}
+
+console.log(calculateDiscount(100, 20));
+console.log(calculateDiscount(50, 150));
+console.log(calculateDiscount(-10, 10));
+console.log(calculateDiscount("100", 10));
