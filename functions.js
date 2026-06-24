e16ae0905d763368841bd04d3775d2fa02d6c4be
Naoman Saeed <naoman.saeed@gmail.com>
2026-06-24 06:52:45 +0500

    looking at the this keyword.

--- functions.js
+++ functions.js
@@ -11,3 +11,29 @@ function processOrder() {
 
 calculateTotal(10, 20);
 processOrder();
+
+const cart = {
+  items: [],
+  add(p) {
+    this.items.push(p);
+  },
+};
+
+cart.add("Apple");
+console.log(cart.items);
+
+const tn = cart.add("Melon");
+console.log(cart.items);
+/*
+const fn = cart.add;
+fn("banana");
+console.log(cart.items); */
+
+const object = {
+  value: 42,
+  log: function () {
+    console.log(this.value);
+  },
+};
+const detached = object.log;
+detached();
