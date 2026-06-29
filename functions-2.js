56c5d6a94d834700aafd019eeefa1040366f87c4
Naoman Saeed <naoman.saeed@gmail.com>
2026-06-29 11:09:11 +0500

    understanding function implementation

--- functions-2.js
+++ functions-2.js
@@ -37,4 +37,21 @@ const applyCoupon = function (cartTotal, couponCode) {
 console.log(applyCoupon(700, "SAVE20"));
 console.log(applyCoupon(413, "SAVE10"));
 console.log(applyCoupon(1026, "MY_EYES!"));
-console.log(applyCoupon((0/0), "SAVE10"));
+//console.log(applyCoupon((0/0), "SAVE10"));
+
+// Function to calculater tax payable given available money & tax rate
+const calculateTax = function (amount, rate) {
+   const taxAmount = amount * (rate/100);
+   return taxAmount;
+};
+
+console.log(calculateTax(55000, 5));
+
+// calculating tip from bill amount and tip-percentage
+
+const calculateTip = function (bill, percent) {
+	const tipAmount = bill * (percent/100);
+	return tipAmount;
+}
+
+console.log(calculateTip(3000,5));
