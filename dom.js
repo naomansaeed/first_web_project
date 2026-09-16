7906ef8367e4c2e94818859c94a79eaa051bab43
Naoman Saeed <naoman.saeed@gmail.com>
2026-09-16 06:12:37 +0500

    practicing DOM traversal in JS.

--- dom.js
+++ dom.js
@@ -17,8 +17,8 @@ const details = document.querySelector(".details");
 //select cancel button
 const cancelBtn = document.getElementById("cancel-btn");
 
-console.log(app.innerHTML);
-console.log(lastItem.innerHTML);
+//console.log(app.innerHTML);
+//console.log(lastItem.innerHTML);
 console.log(fake);
 console.log(inactiveArr);
 //changing the class of status card from pending to success.
@@ -31,3 +31,43 @@ details.textContent = "Your receipt has been emailed.";
 //disable cancel button
 cancelBtn.setAttribute("disabled", "true");
 cancelBtn.textContent = "Done";
+//statusTitle.innerHTML= "<script>alert('hacked')</script>";
+
+// 1. Grab a starting point (the button)
+const editBtn = document.querySelector(".edit-btn");
+
+// 2. Moving UP
+const parentCard = editBtn.parentElement; 
+// Grabs the <div class="card"> wrapping the button
+
+const dashboard = editBtn.closest("#dashboard"); 
+// Travels up past the div, past the section, and stops exactly at <section id="dashboard">
+
+// 3. Moving DOWN (Scoped searching)
+const metricInsideCard = parentCard.querySelector(".metric"); 
+// Only searches INSIDE parentCard, ignoring the second card entirely.
+console.log(metricInsideCard.textContent); // "Score: 100"
+
+// 4. Moving SIDEWAYS
+const heading = parentCard.previousElementSibling; 
+// Grabs the <h2>User Stats</h2> because it sits right above the card in the HTML.
+console.log(heading.textContent);
+
+// ---
+
+const deleteBtn = document.querySelector(".delete-btn");
+
+const parentEl = deleteBtn.parentElement;
+
+const innerSpan = parentEl.querySelector('span');
+
+const sibEl = parentEl.nextElementSibling;
+
+const todoEl = parentEl.closest('#todo-list');
+
+console.log(deleteBtn.textContent);
+console.log(parentEl.textContent);
+console.log(innerSpan.textContent);
+console.log(sibEl.textContent);
+console.log(todoEl.textContent);
+console.log(editBtn.nextSibling.textContent);
